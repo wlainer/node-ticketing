2e8b237e24547ad37522e9f4d1bf58361827ce3e
Wlainer Ribeiro <wlainer@gmail.com>
2020-06-30 08:39:59 +0100

    Adding auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,29 @@
-import express from 'express'
-import {json} from 'body-parser'
+import express from "express";
+import 'express-async-errors'
+import { json } from "body-parser";
 
-const app = express()
-app.use(json())
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+import { errorHandler } from "./middleware/error-handler";
+import {NotFoundError} from "./errors/not-found-error";
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all('*', () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!')
-})
+  console.log("Listening on port 3000!");
+});
