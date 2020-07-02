9f241474c4efba9449561babddee76932bd656f4
Wlainer Ribeiro <wlainer@gmail.com>
2020-07-02 16:19:28 +0100

    Experimenting NATS

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,18 @@
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Listener } from "./base-listener";
+import { Subjects } from "./subjects";
+import { Message } from "node-nats-streaming";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = "payments-service";
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    console.log("Event data: ", data);
+
+    console.log(data.title);
+    console.log(data.price);
+
+    msg.ack();
+  }
+}
